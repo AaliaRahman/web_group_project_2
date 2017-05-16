3dc5546881a13a36585922f8cc544bc90be4a153
AaliaRahman <aaliarahma@gmail.com>
2017-05-16 17:56:31 -0500

    All small JS JQ assignments done at long last. Yay.

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,20 +14,17 @@ $(document).ready(function(){
 //SubmitForm
 //Store name of customer in var. Apply var to alert message.
 //Thank you for your order, name! We will get back to your shortly.
-var name = function(){
-  alert("Thank you for your order, We will get back to you shortly.");
-};
-
-  $('.thankYou').on('click', function(){
-  alert("Thank you for your order, We will get back to you shortly.");
-};
-
-//Form submission
-$('.submitButton').on('click', function(){
-$('.customCakeForm').css({'display':'none'});
-$('.otherMenu').css({'display':'flex'});
+$('#submitButton').on('click', function(){
+var nameElement = document.getElementById('name');
+var name = nameElement.value;
+$('.submission').css({"display": "flex"});
+$('.customCakeForm').css({"display": "none"});
+alert("Thank you for your order, "+ name +"! We will get back to your shortly.")
 });
 
-
+//shrink and fix nav bar on scroll
+$( window ).scroll(function() {
+  $( "header" ).css({'position':'fixed', 'opacity':'0.9', 'z-index': '4', 'width':'100%', 'transform' : 'scaleY(0.9) translateY(-20px)'});
+});
 
 });
